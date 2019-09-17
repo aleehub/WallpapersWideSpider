dd53a27ddf109fc82db61139b51a40a8ec029032
aleehub <462523421@qq.com>
2019-09-17 11:23:03 +0800

    热门壁纸的爬取(优化代码格式)

--- wallpaper.py
+++ wallpaper.py
@@ -1,8 +1,5 @@
-import urllib
 
 import threading
-
-import multiprocessing
 
 import requests
 
@@ -11,46 +8,62 @@ from lxml import etree
 
 def wallpaperlist(url, headers):
 
+    """
+    根据wallpaperswide网页构造，生成热门壁纸的相对名字路径列表
+    :param url: 热门壁纸路径
+    :param headers: 请求头
+    :return: 返回一个下载路径列表
+    """
     response = requests.get(url=url, headers=headers)
 
     content = etree.HTML(response.text)
 
-    href = content.xpath("//div[@class='mini-hud' and @id='hudtitle']/a/@href")
+    name_list = content.xpath("//div[@class='mini-hud' and @id='hudtitle']/a/@href")
 
-    return href
-
-
-def download(href,headers):
-
-    print(href)
-
-    response = requests.get(href[1], headers=headers)
-
-    path = "download/%s.jpg"%href[0]
-
-    write_to_html(path, response)
-
-    print('下载成功:', href[0])
+    return name_list
 
 
+def download(href, header):
 
-def write_to_html(path, response):
+    """
+    根据传入壁纸路径，下载该壁纸
+    :param href:
+    :param header:
+    :return:
+    """
 
-        with open(path, "wb") as f:
+    response = requests.get(href[1], headers=header)
 
-            f.write(response.content)
+    path = "download/%s.jpg" % href[0]
 
-def main(pageNum):
+    write_to_local(path, response)
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36\
-                          (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
-        'Cookie': '_ga=GA1.2.1761384977.1568614064; _gid=GA1.2.409143292.1568614064; __gads=ID=b8f347fe9629b571:T=1568614066:S=ALNI_MZf1DuAq9mGzSvHv-vXE_t6_hgnWw; PHPSESSID=16bd3ec1907411f342a53005b6134d07; ae74935a9f5bd890e996f9ae0c7fe805=q5vS1ldKBFw%3D5bsJAoCRxp0%3D5JiQfeVePKY%3Dl4t%2FkEo5S%2Bc%3Daa0wj%2BrGoS4%3DlopdREWA8%2B4%3DquA2PukvyvY%3DQT%2B7MWP5KJ0%3D'
-    }
+    print('下载成功:%s.jpg' % href[0])
 
-    indexurl = 'http://wallpaperswide.com/top_wallpapers/page/'
 
-    list1 = wallpaperlist(indexurl+str(pageNum), headers)
+def write_to_local(path, response):
+    """
+    将响应内容写入本地
+    :param path: 写入路径
+    :param response: 响应内容
+    :return:
+    """
+    with open(path, "wb") as f:
+
+        f.write(response.content)
+
+
+def link(page_num, header):
+    """
+    生成一个由壁纸名字和路径组成元组的列表
+    :param page_num: 页数
+    :param header:  请求头
+    :return:
+    """
+    # 最热门壁纸的首页路径
+    index_url = 'http://wallpaperswide.com/top_wallpapers/page/'
+
+    list1 = wallpaperlist(index_url+str(page_num), header)
 
     list2 = []
 
@@ -58,41 +71,49 @@ def main(pageNum):
 
         url = 'http://wallpaperswide.com/download'
 
-        index = i.find('-')
-
-
-
         k = i.replace('wallpapers', '1920x1080')
 
-        newurl = url+k
+        new_url = url+k
 
-        list2.append((i[1:index], newurl))
+        index = i.find('-')
+
+        list2.append((i[1:index], new_url))
 
     return list2
 
-if __name__ == '__main__':
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36\
-                          (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
-        'Cookie': '_ga=GA1.2.1761384977.1568614064; _gid=GA1.2.409143292.1568614064; __gads=ID=b8f347fe9629b571:T=1568614066:S=ALNI_MZf1DuAq9mGzSvHv-vXE_t6_hgnWw; PHPSESSID=16bd3ec1907411f342a53005b6134d07; ae74935a9f5bd890e996f9ae0c7fe805=q5vS1ldKBFw%3D5bsJAoCRxp0%3D5JiQfeVePKY%3Dl4t%2FkEo5S%2Bc%3Daa0wj%2BrGoS4%3DlopdREWA8%2B4%3DquA2PukvyvY%3DQT%2B7MWP5KJ0%3D'
-    }
+def main(start, end, header):
 
-    for i in range(11, 100):
+    for i in range(start, end):
 
-        downlist = main(i)
+        print("开始识别第%d页壁纸路径" % i)
 
+        down_list = link(i, header)
 
-        for i in downlist:
+        print("识别完毕，开始下载第%d页壁纸" % i)
 
+        for j in down_list:
 
-            # download(i, headers)
+            print("开始下载:%s.jpg" % j[0])
 
-            thread_download = threading.Thread(target=download, args=(i,headers))
+            thread_download = threading.Thread(target=download, args=(j, header))
 
             thread_download.start()
 
 
+if __name__ == '__main__':
+
+        a = 1
+
+        b = 5
+
+        header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36\
+                              (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
+            'Cookie': '_ga=GA1.2.1761384977.1568614064; _gid=GA1.2.409143292.1568614064; __gads=ID=b8f347fe9629b571:T=1568614066:S=ALNI_MZf1DuAq9mGzSvHv-vXE_t6_hgnWw; PHPSESSID=16bd3ec1907411f342a53005b6134d07; ae74935a9f5bd890e996f9ae0c7fe805=q5vS1ldKBFw%3D5bsJAoCRxp0%3D5JiQfeVePKY%3Dl4t%2FkEo5S%2Bc%3Daa0wj%2BrGoS4%3DlopdREWA8%2B4%3DquA2PukvyvY%3DQT%2B7MWP5KJ0%3D'
+        }
+
+        main(a, b, header)
 
 
 
